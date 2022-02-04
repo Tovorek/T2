3c055ee5b288143d9c9d9e7218a46b54ea37292d
Arrow <71452112+buhuhaha@users.noreply.github.com>
2022-02-04 07:41:11 +0530

    Coding challenge

---
+++ Galaxy of stars.js
@@ -0,0 +1,30 @@
+var ar = getDistinctElements([1, 2, 3, 6, -1, 2, 9, 7, 10, -1, 100]);
+println(ar);
+
+function getDistinctElements(ar)
+{
+    var ar2 = [];
+    
+    var lastIndex = ar.length - 1;
+    
+    for(let i = 0; i <= lastIndex; i++)
+    {
+        if (!isInArray(ar[i], ar, i + 1, lastIndex))
+        {
+            ar2.push(ar[i]);
+        }
+    }
+    
+    return ar2;
+}
+
+function isInArray(n, ar, fromIndex, toIndex)
+{
+    for(var i = fromIndex; i <= toIndex; i++)
+    {
+        if (ar[i] === n)
+            return true;
+    }
+    
+    return false;
+}
