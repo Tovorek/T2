57ab6789535a71b041f729fa3ac8cf88e5e11f5c
Arrow <71452112+buhuhaha@users.noreply.github.com>
2022-05-29 06:46:15 +0530

    js solution

---
+++ single object creation singleton.js
@@ -0,0 +1,21 @@
+var Singleton = (function () {
+    var instance;
+ 
+    function createInstance() {
+        var object = new Object("I am the instance");
+        return object;
+    }
+ 
+    return {
+        getInstance: function () {
+            if (!instance) {
+                instance = createInstance();
+            }
+            return instance;
+        }
+    };
+})();
+
+// driver code
+const instance1 = Singleton.getInstance();
+const instance2 = Singleton.getInstance();
