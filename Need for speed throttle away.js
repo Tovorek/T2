73e78cd1c8725893ea422968454db75965867732
Arrow <71452112+buhuhaha@users.noreply.github.com>
2022-05-31 11:30:38 +0530

    coding challenge

---
+++ Need for speed throttle away.js
@@ -0,0 +1,17 @@
+function throttle(fn, delay, context){
+    let timer;
+    let lastArgs;
+
+    return function(...args){
+        lastArgs = args;
+        context = this ?? context;
+        
+        if(timer) return;
+        
+        timer = setTimeout(()=>{
+            fn.apply(context, lastArgs);
+            clearTimeout(timer);
+        }
+        , delay);
+    };
+}
