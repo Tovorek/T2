c10af7b33232914acca2d152e2fbdb8b4a6ed58d
Arrow <71452112+buhuhaha@users.noreply.github.com>
2022-06-02 19:19:31 +0530

    challenge solution

---
+++ nested mystery.js
@@ -0,0 +1,10 @@
+function keyValuePrinter(obj){
+    for(let key in obj){
+        if(typeof obj[key] !== "object"){
+            console.log("[" + key + " : " + obj[key] + "]");
+        }
+        else{
+            keyValuePrinter(obj[key]);
+        }
+    }
+}
