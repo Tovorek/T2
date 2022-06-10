5a392aebe6367bf44ecba43bebf4112a1988b16d
Arrow <71452112+buhuhaha@users.noreply.github.com>
2022-06-10 05:43:17 +0530

    solution

---
+++ Librarian.js
@@ -0,0 +1,5 @@
+const books = [
+    { name: "Harry Potter", author: "Joanne Rowling" },
+    { name: "Warcross", author: "Marie Lu" },
+    { name: "The Hunger Games", author: "Suzanne Collins" },
+]
