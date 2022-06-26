b4ef7155f8675895c6e5c8c6561ca6ae9b0f6502
Arrow <71452112+buhuhaha@users.noreply.github.com>
2022-06-26 18:32:23 +0530

    Demonstration

---
+++ typeoff.js
@@ -0,0 +1,15 @@
+typeof 50                 
+typeof "text"             
+typeof true               
+typeof undefined          
+typeof function(){}       
+typeof 10n                
+typeof Symbol()           
+typeof [1, 2]             
+typeof {}                 
+
+typeof NaN                
+typeof undeclaredVar      
+typeof Infinity           
+typeof null               
+typeof /regex/  
